706cecbfd2dee833b4ddfa1e3380ecd18f0e790d
Shannon Mulloy <student@epicodus.com>
2015-11-30 18:11:49 -0800

    connect angular frontend to post routes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,14 +1,24 @@
 var app = angular.module('hackerNews', ['ui.router']);
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
-    posts: [
-      {title: 'post 1', upvotes: 5, link: 'https://news.ycombinator.com/', comments: []},
-      {title: 'post 2', upvotes: 2, link: 'https://news.ycombinator.com/', comments: [{author: 'Tom', body: 'Interesting.', upvotes: 2}]},
-      {title: 'post 3', upvotes: 15, link: 'https://news.ycombinator.com/', comments: [{author: 'Mary', body: 'no way', upvotes: 5}]},
-      {title: 'post 4', upvotes: 9, link: 'https://news.ycombinator.com/', comments: [{author: 'Jane', body: 'Bah! Humbug!', upvotes: 10}, {author: 'Kevin', body: 'great. :P', upvotes: 1}]},
-      {title: 'post 5', upvotes: 4, link: 'https://news.ycombinator.com/', comments: [{author: 'Bob', body: 'weird!', upvotes: 2}, {author: 'Joe', body: 'cool post', upvotes: 0}]}
-    ]
+    posts: []
+  };
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts)
+    });
+  };
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data){
+        post.upvotes += 1;
+      });
   };
   return o;
 }])
@@ -20,16 +30,15 @@ app.controller('MainCtrl', [
     $scope.posts = posts.posts;
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
       });
       $scope.title = '';
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
   }
 ]);
@@ -61,7 +70,12 @@ app.config([
       .state('home', {
         url: '/home',
         templateUrl: '/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts', function(posts){
+            return posts.getAll();
+          }]
+        }
       })
       .state('posts', {
         url: '/posts/{id}',
