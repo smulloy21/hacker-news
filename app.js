41bf40295921bb8694f37b06b3ae0c9423f5aa3f
Shannon Mulloy <student@epicodus.com>
2015-11-30 17:43:41 -0800

    add RESTful routes

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
 
 mongoose.connect('mongodb://localhost/news');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
